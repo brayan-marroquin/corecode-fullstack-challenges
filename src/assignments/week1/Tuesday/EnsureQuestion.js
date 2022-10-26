f040861529f83dca0ae72a6cb27f9c0639bc7ce9
brayan-marroquin <70416432+brayan-marroquin@users.noreply.github.com>
2022-10-26 00:14:21 -0600

    add exercise 01 of the day Tuesday of week 01

---
+++ src/assignments/week1/Tuesday/EnsureQuestion.js
@@ -0,0 +1,7 @@
+/*
+Given a string, write a function that returns the string with a question mark ("?") appends to the end, 
+unless the original string ends with a question mark, in which case, returns the original string.
+*/
+function ensureQuestion(s) {
+    return s ? s.endsWith('?') ? s : s + '?' : '?';
+}
